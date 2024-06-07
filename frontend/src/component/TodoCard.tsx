18f2a45557fa7c9823ae92f7b0decd2e746157eb
tingSHI <ting.shi2015@gmail.com>
2024-06-07 15:10:50 +0200

    - Add Put function (but low performance: send PUT Request via each taste click)

--- frontend/src/component/TodoCard.tsx
+++ frontend/src/component/TodoCard.tsx
@@ -1,6 +1,7 @@
 import {Todo} from "../type/Todo.ts";
 import './TodoCard.css'
 import axios from "axios";
+import {ChangeEvent, useState} from "react";
 
 type TodoCardProps = {
     todo: Todo,
@@ -10,6 +11,8 @@ type TodoCardProps = {
 
 export default function TodoCard(props: TodoCardProps){
 
+    const [description, setDescription] = useState<string>(props.todo.description)
+
     const deleteThisItem = ()=>{
         axios.delete("api/todo/" + props.todo.id)
             .then(props.onTodoItemChange)
@@ -17,12 +20,23 @@ export default function TodoCard(props: TodoCardProps){
             .finally(()=>console.log("Finally-Delete"))
     }
 
+    const changeText = (event:ChangeEvent<HTMLInputElement>)=>{
+        const newDescription = event.target.value
+        setDescription(newDescription)
+        axios.put("api/todo/" + props.todo.id, {
+            id: props.todo.id,
+            description:newDescription,
+            status: props.todo.status,
+            } as Todo)
+            .then(props.onTodoItemChange)
+            .catch(error => console.log(error))
+            .finally(()=>"Finally-Put")
+    }
+
 
     return(
         <div className="todo-card">
-            {
-                props.todo.description
-            }
+            <input value={description} onInput={changeText}/>
             <button onClick={deleteThisItem}>🚮</button>
         </div>
     )
